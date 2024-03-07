f42564ac274b38184cb161e76269d2537d744b34
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-03-08 01:24:09 +0500

    gotta complete the layout

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../Container";
-
-export default function Footer() {
-  return (
-    <footer>
-      <Container>Footer</Container>
-    </footer>
-  );
-}
--- src/pages/Students/Students.tsx
+++ src/pages/Students/Students.tsx
@@ -1,13 +1,9 @@
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
 import StudentsMain from "./StudentsMain";
 
 export default function Students() {
   return (
     <>
-      <Header />
       <StudentsMain />
-      <Footer />
     </>
   );
 }
