f3bbd34376bea3735bc86a27bf24a4202f7711b2
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-03-09 18:32:14 +0500

    gave the menu some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      backgroundImage: {
+        "sidebar-menu-gradient":
+          "linear-gradient(0deg, rgba(146,95,226,1) 0%, rgba(147,96,227,1) 100%)",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
