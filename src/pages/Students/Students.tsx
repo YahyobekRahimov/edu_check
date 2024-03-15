76f13a8c54073c8508a7832b6072cc8416fa0fbc
RahmatulloDev <rmamatqulov107@gmail.com>
2024-03-15 11:26:11 +0500

    msg

--- src/pages/Students/Students.tsx
+++ src/pages/Students/Students.tsx
@@ -1,3 +1,64 @@
+
+
+
+import { AutoComplete, Input } from 'antd';
+import type { SelectProps } from 'antd';
+
+
+const getRandomInt = (max: number, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const searchResult = (query: string) =>
+  new Array(getRandomInt(5))
+    .join('.')
+    .split('.')
+    .map((_, idx) => {
+      const category = `${query}${idx}`;
+      return {
+        value: category,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <span>
+              Found {query} on{' '}
+              <a
+                href={`https://s.taobao.com/search?q=${query}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {category}
+              </a>
+            </span>
+            <span>{getRandomInt(200, 100)} results</span>
+          </div>
+        ),
+      };
+    });
+
+const App: React.FC = () => {
+  const [options, setOptions] = useState<SelectProps<object>['options']>([]);
+
+  const handleSearch = (value: string) => {
+    setOptions(value ? searchResult(value) : []);
+  };
+
+  const onSelect = (value: string) => {
+    console.log('onSelect', value);
+  };
+
+
+};
+
+
+
+
+
+
+
+
 import {
   Button,
   Dropdown,
@@ -6,6 +67,8 @@ import {
   TableProps,
   Tag,
 } from "antd";
+import React, { useState } from 'react';
+
 
 import { ColumnType } from "antd/es/table";
 import { MenuProps } from "antd/lib";
@@ -151,7 +214,23 @@ export default function Students() {
 
   return (
     <main>
+            <Input.Search size="large" placeholder="Ismni kiriting"  enterButton />
+
+
+
+
+
+
+
+
+
+
+
       <Table dataSource={dataSource} columns={columns} />
+     
+   
+      
+    
     </main>
   );
 }
