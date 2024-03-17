d56ed64aedf4766488f31f49e8ed5cdf51f34b6e
FayzullayevAbbos <abbosfayzullayev202@gmail.com>
2024-03-17 14:13:13 +0500

    single page

--- src/pages/Groups/Group/Group.tsx
+++ src/pages/Groups/Group/Group.tsx
@@ -1,3 +1,36 @@
+import { Switch, Table } from "antd";
 export default function Group() {
-  return <>Hello</>;
+  const colums = [
+    {
+      title: "F.I.Sh",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "To'lovlar",
+      dataIndex: "payments",
+      key: "payments",
+    },
+    {
+      title: "Davomat",
+      dataIndex: "davomat",
+      key: "davomat",
+      render: () => {
+        return <Switch  defaultChecked />;
+      },
+    },
+  ];
+  const data = [
+    {
+      fullName: "Rustamov D",
+      payments: "ok",
+    },
+  ];
+  return (
+    <>
+      <div className="bg-[var(--dark-backround)] w-full">
+        <Table columns={colums} dataSource={data} />
+      </div>
+    </>
+  );
 }
