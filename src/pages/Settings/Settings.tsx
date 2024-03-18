fe25d4e028a776a9cdefbb8fbbf8f6d7e98f4834
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-03-19 02:20:53 +0500

    changed the color

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,3 +1,14 @@
 export default function Settings() {
-  return <div>Settings</div>;
+  return (
+    <div className="p-10">
+      <div className="max-w-[200px] max-h-[200px] overflow-hidden rounded-full pointer-events-none">
+        <img
+          src="https://picsum.photos/200/200"
+          alt="Avatar photo"
+          className="pointer-events-none"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 }
